ba5b9f42e98fa109f2107ec6bf247be604d9b687
Balan Artiom <artiombalan331@gmail.com>
2022-12-01 14:07:50 +0200

    combined into single file

--- 2022/01/main.py
+++ 2022/01/main.py
@@ -5,11 +5,17 @@ def format_input(inp):
     inp = [[int(x) for x in s.splitlines()] for s in inp]
     return inp
 
-def sol(inp):
+def sol1(inp):
     inp = format_input(inp)
-    # ss = [sum(s) for s in inp]
     ss = map(sum, inp)
     print(max(ss))
 
-t = open("test.txt", 'r').read()
-sol(t)
+def sol2(inp):
+    inp = format_input(inp)
+    ss = map(sum, inp)
+    from heapq import nlargest
+    print(sum(nlargest(3, ss)))
+
+t = open("input.txt", 'r').read()
+sol1(t)
+sol2(t)
