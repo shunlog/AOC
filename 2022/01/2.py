b68885089fc29321ee31b5c88beb5d6742a08d3f
Balan Artiom <artiombalan331@gmail.com>
2022-12-01 07:46:20 +0200

    2022 day 1; separate input formatting from solution

--- 2022/01/2.py
+++ 2022/01/2.py
@@ -1,11 +1,15 @@
 #!/bin/env python3
 
-def sol(inp):
+def format_input(inp):
     inp = inp.split('\n\n')
-    inp = [s.splitlines() for s in inp]
-    ss = [sum([int(x) for x in s]) for s in inp]
+    inp = [[int(x) for x in s.splitlines()] for s in inp]
+    return inp
+
+def sol(inp):
+    inp = format_input(inp)
+    ss = map(sum, inp)
     st = sorted(ss)[::-1]
     print(sum(st[:3]))
 
-t = open("input.txt", 'r').read()
+t = open("test.txt", 'r').read()
 sol(t)
