6e04254b011679000121436d8c1d7962b64c4efe
Balan Artiom <artiombalan331@gmail.com>
2022-12-02 23:29:42 +0200

    day 2; more sensible var names

--- 2022/02/main.py
+++ 2022/02/main.py
@@ -19,57 +19,23 @@ from icecream import ic
 
 def format_input(inp):
     inp = [(l.split()) for l in inp.splitlines()]
-    ic(inp)
     inp = [(ord(l[0])-ord('A'), ord(l[1])-ord('X')) for l in inp]
-    ic(inp)
     return inp
 
-wt = [0, 3, 6]
-p2sh = [2, 0, 1]
-stw = [1, 2, 0]
+ots = [0, 3, 6] # outcome to score
+otsh = [2, 0, 1] # outcome to shift
+shto = [1, 2, 0] # shift to outcome
 
-def score1(p):
-    sh = (p[1] - p[0]) % 3
-    ws = wt[stw[sh]]
-    ic(ws)
-    return ws + p[1] + 1
+def p1(inp):
+    s = lambda p: ots[shto[(p[1] - p[0]) % 3]] + p[1] + 1
+    return sum(map(s, inp))
 
-def score2(p):
-    ic(p)
-    w = wt[p[1]]
-    ic(w)
-    p2 = (p[0] + p2sh[p[1]]) % 3 + 1
-    ic(p2)
-    return w + p2
-
-
-def sol(inp, part2=False):
-    s = 0
-    if not part2:
-        return sum(map(score1, inp))
-    else:
-        return sum(map(score2, inp))
-
-def test():
-    inp = open("test.txt", 'r').read()
-    inp = format_input(inp)
-    print(sol(inp))
-    # print(sol(inp, True))
-
-def p2():
-    ic.disable()
-    inp = open("input.txt", 'r').read()
-    inp = format_input(inp)
-    print(sol(inp, True))
-
-
-def p1():
-    ic.disable()
-    inp = open("input.txt", 'r').read()
-    inp = format_input(inp)
-    print(sol(inp))
+def p2(inp):
+    s = lambda p: ots[p[1]] + (p[0] + otsh[p[1]]) % 3 + 1
+    return sum(map(s, inp))
 
 if __name__ == "__main__":
-    # test()
-    p1()
-    # p2()
+    inp = open("input.txt", 'r').read()
+    inp = format_input(inp)
+    print(p1(inp))
+    print(p2(inp))
