dfd54148913e47e6094089dd5cec907e7a4f4a87
Balan Artiom <artiombalan331@gmail.com>
2022-12-02 07:44:03 +0200

    combine parts

--- 2022/02/main.py
+++ 2022/02/main.py
@@ -4,27 +4,39 @@ from icecream import ic
 def format_input(inp):
     return inp.splitlines()
 
+def score1(l):
+    s = ord(l[2]) - ord('X') + 1
+    if l in ['A Z', 'C Y', 'B X']:
+        return 0 + s
+    elif ord(l[0])-ord('A') == ord(l[2])-ord('X'):
+        return 3 + s
+    else:
+        return 6 + s
+
+def score2(l):
+    t = [[3, 1, 2],
+         [1, 2, 3],
+         [2, 3, 1]]
+    s = t[ord(l[2])-ord('X')][ord(l[0])-ord('A')]
+    if l[2] == 'X':
+        s += 0
+    if l[2] == 'Y':
+        s += 3
+    if l[2] == 'Z':
+        s += 6
+    ic(s)
+    return s
+
 def sol(inp, part2=False):
     ic(inp)
     s = 0
-    for l in inp:
-        ic(l)
-        t = [[3, 1, 2],
-             [1, 2, 3],
-             [2, 3, 1]]
-        s += t[ord(l[2])-ord('X')][ord(l[0])-ord('A')]
-        if l[2] == 'X':
-            s += 0
-        if l[2] == 'Y':
-            s += 3
-        if l[2] == 'Z':
-            s += 6
-        ic(s)
-    return s
+    if not part2:
+        return sum(map(score1, inp))
+    else:
+        return sum(map(score2, inp))
 
 if __name__ == "__main__":
-    inp = open("input.txt", 'r').read()
+    inp = open("test.txt", 'r').read()
     inp = format_input(inp)
-    # ic.disable()
     print(sol(inp))
-    # print(sol(inp, True))
+    print(sol(inp, True))
