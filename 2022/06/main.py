34fe069d156bf39fe1fe8f7ac4219c27d434ef93
Balan Artiom <artiombalan331@gmail.com>
2022-12-06 09:33:41 +0200

    combined parts

--- 2022/06/main.py
+++ 2022/06/main.py
@@ -1,36 +1,18 @@
 #!/bin/env python3
 from icecream import ic
 
-def p1(inp):
+def solve(inp, n):
     from collections import Counter
     c = Counter()
-
     for i, ch in enumerate(inp):
         c[ch] += 1
-        if i > 3:
-            c[inp[i-4]] -= 1
-            if c[inp[i-4]] == 0:
-                del c[inp[i-4]]
+        if i >= n:
+            c[inp[i-n]] -= 1
+            if c[inp[i-n]] == 0:
+                del c[inp[i-n]]
         ic(c)
-        if len(c.keys()) == 4:
+        if len(c.keys()) == n:
             return i + 1
-
-    return -1
-
-def p2(inp):
-    from collections import Counter
-    c = Counter()
-
-    for i, ch in enumerate(inp):
-        c[ch] += 1
-        if i > 13:
-            c[inp[i-14]] -= 1
-            if c[inp[i-14]] == 0:
-                del c[inp[i-14]]
-        ic(c)
-        if len(c.keys()) == 14:
-            return i + 1
-
     return -1
 
 if __name__ == "__main__":
@@ -39,6 +21,6 @@ if __name__ == "__main__":
     if not '--debug' in sys.argv:
         ic.disable()
     if '2' not in sys.argv:
-        print(p1(inp))
+        print(solve(inp, 4))
     if '1' not in sys.argv:
-        print(p2(inp))
+        print(solve(inp, 14))
