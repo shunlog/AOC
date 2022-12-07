8193634f68ece06d0db26a0080746fdd406eb4c4
Balan Artiom <artiombalan331@gmail.com>
2022-12-07 09:13:01 +0200

    clean up

--- 2022/07/main.py
+++ 2022/07/main.py
@@ -4,58 +4,34 @@ from itertools import *
 from more_itertools import *
 from collections import *
 
+def get_sizes(inp):
+    inp = inp.splitlines()
+    sizes = defaultdict(int)
+    dirs = ['/']
+    for l in inp[1:]:
+        t = l.split()
+        if ['$', 'cd'] == t[:2] and t[2] == '..':
+           dirs.pop()
+        elif ['$', 'cd'] == t[:2] and t[2] != '..':
+           dirs.append(dirs[-1]+t[2]+"/")
+        elif '$' != t[0] and t[0] != 'dir':
+           s = int(t[0])
+           for d in dirs:
+               sizes[d] += s
+    return sizes
+
 def p1(inp):
     MAX = 100000
-    inp = inp.splitlines()
-    dirs = []
-    size = defaultdict(int)
-    for l in inp:
-        t = l.split()
-        if t[0] == '$':
-           if t[1] == 'cd':
-               if t[2] == '..':
-                   dirs.pop()
-               else:
-                   dirs.append("/".join(dirs)+t[2])
-        elif t[0] == 'dir':
-           pass
-        else:
-           s = int(t[0])
-           assert(len(dirs) > 0)
-           for d in dirs:
-               size[d] += s
-    ic(size)
-
-    return sum([s for d,s in size.items() if s <= MAX])
+    return sum([s for d,s in get_sizes(inp).items() if s <= MAX])
 
 def p2(inp):
     TOTAL= 70000000
     NEED = 30000000
-
-    inp = inp.splitlines()
-    dirs = []
-    size = defaultdict(int)
-    for l in inp:
-        t = l.split()
-        if t[0] == '$':
-           if t[1] == 'cd':
-               if t[2] == '..':
-                   dirs.pop()
-               else:
-                   dirs.append("/".join(dirs)+t[2])
-        elif t[0] == 'dir':
-           pass
-        else:
-           s = int(t[0])
-           assert(len(dirs) > 0)
-           for d in dirs:
-               size[d] += s
-    USED = size['/']
-    ic(USED)
+    sizes = get_sizes(inp)
+    USED = sizes['/']
     UNUSED = TOTAL - USED
     DEL = NEED - UNUSED
-
-    return min(filter(lambda s: s >= DEL, size.values()))
+    return min(filter(lambda s: s >= DEL, sizes.values()))
 
 if __name__ == "__main__":
     import sys
