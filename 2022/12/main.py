04307c92bcd9d7288373f5becbe4288755466e8e
Balan Artiom <artiombalan331@gmail.com>
2022-12-12 08:26:53 +0200

    shitty part 2

--- 2022/12/main.py
+++ 2022/12/main.py
@@ -12,6 +12,7 @@ def easy(h1, h2):
 def p1(inp):
     mp = []
     start = fin = None
+    starts = []
     for y,l in enumerate(inp.splitlines()):
         mp.append([])
         for x,ch in enumerate(l):
@@ -23,6 +24,8 @@ def p1(inp):
                 fin = (x,y)
                 mp[y].append('z')
                 continue
+            if ch in ['a', 'S']:
+                starts.append((x,y))
             mp[y].append(ch)
     assert start
     assert fin
@@ -67,14 +70,79 @@ def p1(inp):
     for e in edges:
         g.add_edge(e[0], e[1], e[2])
 
-    ic(edges)
-    ic(start, fin)
     d = dijkstra.DijkstraSPF(g, start)
     dist = d.get_distance(fin)
     return dist
 
 def p2(inp):
-    return 0
+    mp = []
+    start = fin = None
+    starts = []
+    for y,l in enumerate(inp.splitlines()):
+        mp.append([])
+        for x,ch in enumerate(l):
+            if ch == 'S':
+                start = (x,y)
+                mp[y].append('a')
+                continue
+            if ch == 'E':
+                fin = (x,y)
+                mp[y].append('z')
+                continue
+            if ch in ['a', 'S']:
+                starts.append((x,y))
+            mp[y].append(ch)
+    assert start
+    assert fin
+
+    edges = []
+    for y,l in enumerate(mp):
+        for x,h in enumerate(l):
+            try:
+                hd = mp[y][x+1]
+                if easy(h, hd):
+                    edges.append(((x,y), (x+1,y),1))
+                    ic(h, hd)
+            except:
+                pass
+            try:
+                hd = mp[y+1][x]
+                if easy(h, hd):
+                    edges.append(((x,y), (x,y+1),1))
+                    ic(h, hd)
+            except:
+                pass
+            try:
+                if x == 0:
+                    raise
+                hd = mp[y][x-1]
+                if easy(h, hd):
+                    edges.append(((x,y), (x-1,y),1))
+                    ic(h, hd)
+            except:
+                pass
+            try:
+                if y == 0:
+                    raise
+                hd = mp[y-1][x]
+                if easy(h, hd):
+                    edges.append(((x,y), (x,y-1),1))
+                    ic(h, hd)
+            except:
+                pass
+
+    g = dijkstra.Graph()
+    for e in edges:
+        g.add_edge(e[0], e[1], e[2])
+
+    mind = 100000000000
+
+    for start in starts:
+        d = dijkstra.DijkstraSPF(g, start)
+        dist = d.get_distance(fin)
+        mind = min(mind, dist)
+
+    return mind
 
 if __name__ == "__main__":
     import sys
