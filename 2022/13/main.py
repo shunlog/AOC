748da02dbcc8d3a89a620227fa57edb60384ba30
Balan Artiom <artiombalan331@gmail.com>
2022-12-13 19:08:35 +0200

    day 13 part 1

--- 2022/13/main.py
+++ 2022/13/main.py
@@ -1,40 +1,49 @@
 #!/bin/env python3
 from icecream import ic
 
-# True if l1 < l2
-# False otherwise
+# -1 if l1 < l2
+# 0 if l1 = l2
+# 1 if l1 > l2
+# 3379 too low
+
+def cmp_ints(i1, i2):
+    if i1 < i2:
+        return -1
+    elif i1 == i2:
+        return 0
+    else:
+        return 1
 
 def cmp_lists(l1, l2):
-    return True
+    for e1, e2 in zip(l1, l2):
+        r = cmp_expr(e1, e2)
+        if r != 0:
+            return r
+    return cmp_ints(len(l1), len(l2))
 
 def cmp_expr(e1, e2):
     if type(e1) == type(e2) == list:
         return cmp_lists(e1, e2)
     elif type(e1) == type(e2) == int:
-        return e1 < e2
+        return cmp_ints(e1, e2)
     else:
-        if type(e1) == int:
-            e1 = [e1]
-        else:
-            e2 = [e2]
-        return cmp_lists(e1, e2)
+        return cmp_lists([e1] if type(e1) == int else e1,
+                         [e2] if type(e2) == int else e2)
 
-def parse(inp):
+def p1(inp):
     pairs = []
     for p in inp.split('\n\n'):
         pairs.append([eval(l) for l in p.splitlines()])
-    return pairs
-
-def p1(inp):
-    inp = parse(inp)
     s = 0
-    for i, p in enumerate(inp):
+    for i, p in enumerate(pairs):
         ic(p[0], p[1])
-        if cmp_expr(p[0], p[1]):
+        c = cmp_expr(p[0], p[1])
+        if c == -1:
             s += i+1
     return s
 
 def p2(inp):
+    ic(inp.splitlines())
     return 0
 
 if __name__ == "__main__":
