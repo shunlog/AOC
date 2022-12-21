ad2a3a2be9cd182bd13f281e4d352735ceb198e1
Balan Artiom <artiombalan331@gmail.com>
2022-12-21 20:18:01 +0200

    another crack at day 19

--- 2022/19/main.py
+++ 2022/19/main.py
@@ -6,14 +6,57 @@ import math
 def ints(s):
     return [int(i) for i in re.findall(r'-?\d+', s)]
 
+cnt = 0
+
 from collections import defaultdict
 mem = None
 
-def bfs(bp, ores, bots, t):
+def dfs(bpl, ores, bots, t):
     '''Return the maximum number of geodes produced from current time till end'''
+    global cnt
+    cnt += 1
     if t > 24:
         return ores
-    return ores
+
+    # memoize
+    for memores in mem[tuple([tuple(bots), t])]:
+        if all(a >= b for a,b in zip(memores, ores)):
+            return (-1, -1, -1, -1)
+    else:
+        mem[tuple([tuple(bots), t])].append(tuple(ores))
+
+    bestores = ores
+    buybotsl = [[0, 0, 0, 1], [0, 0, 1, 0], [0, 1, 0, 0], [1, 0, 0, 0], [0, 0, 0, 0]]
+    for buybots in buybotsl:
+        # check if robot is not required
+        try:
+            bi = buybots.index(1)
+            notreq = False
+            if all(bpl[bi][o] <= bots[o] for o in range(3)):
+                ic(bots, bp, buybots)
+                notreq = True
+            if notreq:
+                continue
+        except:
+            pass
+        # buy bot
+        cost = [0, 0, 0, 0]
+        for bi,buybot in enumerate(buybots):
+            if buybot == 0:
+                continue
+            cost = [a + b for a,b in zip(cost, [pr * buybot for pr in bpl[bi]])]
+        if any(ores[i] < cost[i] for i in range(4)):
+            continue
+        # collect res
+        newores = [a + b - c for a,b,c in zip(ores, bots, cost)]
+        # get bot
+        newbots = [a + b for a,b in zip(bots, buybots)]
+        # dfs
+        nextores = dfs(bpl, newores, newbots, t+1)
+        if nextores[-1] > bestores[-1]:
+            bestores = nextores
+
+    return bestores
 
 
 def p1(inp):
@@ -25,13 +68,18 @@ def p1(inp):
         bpl.append(((o1, 0, 0, 0), (c1, 0, 0, 0),
                     (ob1, ob2, 0, 0), (g1, 0, g3, 0)))
 
-    for bp in bpl:
-        global mem
-        mem = defaultdict(lambda: tuple([-1, -1, -1, -1]))
-        v = bfs(bp, (0, 0, 0, 0), (1, 0, 0, 0), 1)
+    q = 0
+    for i, bp in enumerate(bpl):
+        global mem, cnt
+        mem = defaultdict(list)
+        cnt = 0
+        v = dfs(bp, (0, 0, 0, 0), (1, 0, 0, 0), 1)
+        q += v[-1] * (i+1)
+        # ic(mem)
         ic(bp, v)
+        ic(cnt)
 
-    return 0
+    return q
 
 def p2(inp):
     return 0
