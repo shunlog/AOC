5dd483267ac64fe6d050f97c4a0eace90ffca63a
shunlog <artiombalan331@gmail.com>
2023-12-03 15:57:38 +0200

    improve day3

--- 2023/03/main.py
+++ 2023/03/main.py
@@ -3,21 +3,51 @@ from icecream import ic
 import re
 from collections import defaultdict
 import math
+from collections import namedtuple
 
-
-def adjacent_to_symbol(x, y, m):
+def neighbors(m, x, y):
+    # Input: matrix and a pair of coordinates
+    # Output: list of all neighboring cells: Cell(val, x, y)
+    Cell = namedtuple('Cell', ['val', 'x', 'y'])
+    l = []
     for dx in (-1, 0, 1):
         for dy in (-1, 0, 1):
-            if dx == 0 and dy == 0:  # the digit itself
-                continue
-            try: # check if out of bounds
+            if dx == 0 and dy == 0:
+                continue # same cell
+            try:
                 m[y+dy][x+dx]
             except:
-                continue
-            ch = m[y+dy][x+dx]
-            if not ch.isnumeric() and ch != '.': # char is a symbol
+                continue # out of bounds
+            nx, ny = x+dx, y+dy
+            v = m[ny][nx]
+            l.append(Cell(v, nx, ny))
+    return l
+
+
+
+def p1(inp):
+    # 1. Go through each digit of each number,
+    # 2. Check all neighboring sympols
+    # 3. If found, add number to sum
+    def adjacent_to_symbol(x, y, m):
+        for cell in neighbors(m, x, y):
+            if not cell.val.isnumeric() and cell.val != '.': # char is a symbol
                 return True
-    return False
+        return False
+
+    m = inp.splitlines() # matrix
+    p = re.compile(r'\d+')
+
+    s = 0
+    for y, l in enumerate(m):
+        for match in p.finditer(l): # check each number
+            ic(match)
+            for x in range(*match.span()): # check each digit
+                ic(m[y][x])
+                if adjacent_to_symbol(x, y, m): # is engine part
+                    s += int(match[0])
+                    break
+    return s
 
 
 def p2(inp):
@@ -30,18 +60,10 @@ def p2(inp):
     def neighb_gears(x, y):
         # return set of neighb. gears
         g = set()
-        for dx in (-1, 0, 1):
-            for dy in (-1, 0, 1):
-                if dx == 0 and dy == 0:
-                    continue # the digit itself
-                try:
-                    m[y+dy][x+dx]
-                except:
-                    continue # out of bounds
-                ch = m[y+dy][x+dx]
-                if ch != '*':
-                    continue # not a gear
-                g.add((x+dx, y+dy))
+        for cell in neighbors(m, x, y):
+            if cell.val != '*':
+                continue # not a gear
+            g.add((cell.x, cell.y))
         return g
 
 
@@ -60,23 +82,12 @@ def p2(inp):
     return sum(math.prod(int(n) for n in v) for v in gears.values() if len(v) == 2)
 
 
+
 def solve(inp, part2=False):
     if part2:
         return p2(inp)
-    m = inp.splitlines() # matrix
-    p = re.compile(r'\d+')
 
-    s = 0
-    for y, l in enumerate(m):
-        for match in p.finditer(l): # check each number
-            ic(match)
-            for x in range(*match.span()): # check each digit
-                ic(m[y][x])
-                if adjacent_to_symbol(x, y, m): # is engine part
-                    s += int(match[0])
-                    break
-
-    return s
+    return p1(inp)
 
 
 if __name__ == "__main__":
