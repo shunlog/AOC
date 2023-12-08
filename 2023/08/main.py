d6214c8c7dac947760b4ff1ef402d131eae84d9c
shunlog <artiombalan331@gmail.com>
2023-12-08 20:32:24 +0200

    washed day 8

--- 2023/08/main.py
+++ 2023/08/main.py
@@ -5,7 +5,10 @@ import re
 from math import lcm
 
 
-def p1(steps, d, node='AAA', end=lambda n: n == 'ZZZ'):
+def period(steps, d, node='AAA', end=lambda n: n == 'ZZZ'):
+    '''Number of steps until a node reaches from start to end,
+    assuming it indeed is a cycle.
+    '''
     for i, s in enumerate(cycle(steps), start=1):
         node = d[node][0] if s == 'L' else d[node][1]
         if end(node):
@@ -14,32 +17,22 @@ def p1(steps, d, node='AAA', end=lambda n: n == 'ZZZ'):
     assert False
 
 
-def p2(steps, d):
-    # find all nodes that end in "A"
-    nodes = [n for n in d.keys() if n[-1] == 'A']
-    ic(nodes)
-
-    periods = [p1(steps, d, n, lambda n: n[-1] == 'Z') for n in nodes]
-
-
-    return lcm(*periods)
-
-
 def solve(inp, part2=False):
     lines = inp.splitlines()
     steps = lines[0]
-    ic(steps)
 
     r = re.compile(r'(\S+) = \((\S+), (\S+)\)')
-
     d = dict()
     for l in lines[2:]:
         k, vl, vr = r.match(l).groups()
         d[k] = (vl, vr)
 
     if not part2:
-        return p1(steps, d)
-    return p2(steps, d)
+        return period(steps, d)
+
+    nodes = [n for n in d.keys() if n[-1] == 'A'] # all nodes that end in "A"
+    periods = [period(steps, d, n, lambda n: n[-1] == 'Z') for n in nodes]
+    return lcm(*periods)  # I don't know why this necessarily works
 
 
 if __name__ == "__main__":
