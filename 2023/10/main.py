97c01c512b29d344b4ad598f61c6bbbbf064969a
shunlog <artiombalan331@gmail.com>
2023-12-10 17:19:54 +0200

    clean up part 1

--- 2023/10/main.py
+++ 2023/10/main.py
@@ -2,69 +2,62 @@
 from icecream import ic
 
 
-def find_S(m):
-    for y, row in enumerate(m):
-        for x, ch in enumerate(row):
-            if ch == 'S':
-                return (x, y)
+def find_path(m):
+    '''Given the matrix, return a list of coordinates are stored as tuples (x, y)
+    which represents the closed cycle starting from S.'''
+    def find_S(m):
+        for y, row in enumerate(m):
+            for x, ch in enumerate(row):
+                if ch == 'S':
+                    return (x, y)
 
+    def find_next_pipe(m, pos, prev=None):
+        exits = { # the directions in which you can exit the pipe
+            '-': ((1, 0), (-1, 0)),
+            '|': ((0, 1), (0, -1)),
+            'F': ((1, 0), (0, 1)),
+            'L': ((1, 0), (0, -1)),
+            'J': ((0, -1), (-1, 0)),
+            '7': ((0, 1), (-1, 0)),
+        }
+        x, y = pos
+        ch = m[y][x]
 
-def find_next_pipe(m, pos, prev=None):
-    enters = { # the directions in which you can enter the pipe
-        '-': ((1, 0), (-1, 0)),
-        '|': ((0, 1), (0, -1)),
-        'F': ((-1, 0), (0, -1)),
-        'L': ((0, 1), (-1, 0)),
-        'J': ((0, 1), (1, 0)),
-        '7': ((0, -1), (1, 0)),
-        'S': ((-1, 0), (1, 0), (0, -1), (0, 1))
-    }
-    exits = { # the directions in which you can exit the pipe
-        '-': ((1, 0), (-1, 0)),
-        '|': ((0, 1), (0, -1)),
-        'F': ((1, 0), (0, 1)),
-        'L': ((1, 0), (0, -1)),
-        'J': ((0, -1), (-1, 0)),
-        '7': ((0, 1), (-1, 0)),
-        'S': ((-1, 0), (1, 0), (0, -1), (0, 1))
-    }
+        def can_enter(dx, dy):
+            '''Return True if you can go in the given direction from current pos'''
+            nch = m[y+dy][x+dx]
+            if nch == '.':
+                return False
+            return (-dx, -dy) in exits[nch]
 
-    x, y = pos
-    ch = m[y][x]
-    for dx, dy in ((-1, 0), (1, 0), (0, -1), (0, 1)):
-        nx, ny = x + dx, y + dy
-        if (nx, ny) == prev:
-            continue
-        try:
-            nch = m[ny][nx]
-        except:
-            continue
-        if (dx, dy) not in exits[ch]: # can't exit the pipe in that direction
-            continue
-        if nch != '.' and (dx, dy) in enters[nch]:
-            return (nx, ny)
+        # if on 'S', go wherever you can enter the pipe
+        if ch == 'S':
+            for dx, dy in ((-1, 0), (0, 1), (1, 0), (0, -1)):
+                if can_enter(dx, dy):
+                    return (x+dx, y+dy)
+
+         # else, just go in the direction that doesn't lead to `prev`
+        for dx, dy in exits[ch]:
+            nx, ny = x+dx, y+dy
+            if (nx, ny) == prev:
+                continue
+            return nx, ny
+
+    S = find_S(m)
+    path = [S, find_next_pipe(m, S)]
+    while path[-1] != S:
+        npos = find_next_pipe(m, path[-1], path[-2])
+        path.append(npos)
+
+    return path
 
 
 def solve(inp, part2=False):
     m = inp.splitlines()
-    S = find_S(m)
+    path = find_path(m)
 
-    path = []
-    pos = S
-    prev = None
-    ic(pos)
-    while True:
-        path.append(pos)
-        prev, pos = pos, find_next_pipe(m, pos, prev)
-        ch = m[pos[1]][pos[0]]
-        prev_ch = m[prev[1]][prev[0]]
-        # ic(prev_ch, ch)
-        if ch == 'S':
-            break
-
-    ic(len(path))
-
-    return len(path) // 2
+    if not part2:
+        return len(path) // 2
 
 
 if __name__ == "__main__":
