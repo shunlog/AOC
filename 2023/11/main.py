7467122d425de85631be2e0f173fbd272e0006b9
shunlog <artiombalan331@gmail.com>
2023-12-11 10:45:07 +0200

    clean up a bit

--- 2023/11/main.py
+++ 2023/11/main.py
@@ -1,23 +1,15 @@
 #!/bin/env python3
 from icecream import ic
 from itertools import combinations
+from functools import cache
 
-
-def solve(inp, part2=False):
-    expansion = 1000000-1 if part2 else 1
-
-    m = [list(row) for row in inp.splitlines()]
+@cache
+def compute_actual_dist(m, expansion):
+    '''For any x and y on which there is a galaxy,
+    actual_x[x] = actual x after expansion
+    actual_y[y] = actual y after expansion
+    '''
     w, h = len(m[0]), len(m)
-
-    rows_empty = set(y for y, r in enumerate(m) if all(ch == '.' for ch in r))
-    cols_empty = set(x for x in range(w) if all(m[y][x] == '.' for y in range(h)))
-
-    galaxies = {(x, y) for y, r in enumerate(m) for x, ch in enumerate(r) if ch == '#'}
-    ic(galaxies)
-    ic(len(list(combinations(galaxies, 2))))
-
-    # for any x on which there is a galaxy,
-    # actual_x[x] = actual distance from 0
     actual_y = {}
     offset = 0
     for y, row in enumerate(m):
@@ -34,18 +26,21 @@ def solve(inp, part2=False):
         else:
             actual_x[x] = x + offset
 
+    return actual_x, actual_y
 
-    s = 0
-    for g1, g2 in combinations(galaxies, 2):
-        dx = abs(actual_x[g1[0]] - actual_x[g2[0]])
-        dy = abs(actual_y[g1[1]] - actual_y[g2[1]])
-        dist = dx + dy
-        s += dist
 
-    # for each pair of galaxies: 96141
-    # * for each pair of rows + columns:  combi(140, 2) = 9730 * 2 = 18k
+def manhattan_dist(m, expansion, g1, g2):
+    actual_x, actual_y = compute_actual_dist(m, expansion)
+    dx = abs(actual_x[g1[0]] - actual_x[g2[0]])
+    dy = abs(actual_y[g1[1]] - actual_y[g2[1]])
+    return dx + dy
 
-    return s
+
+def solve(inp, part2=False):
+    expansion = 1000000-1 if part2 else 1
+    m = tuple(inp.splitlines())  # needs to be hashable
+    galaxies = {(x, y) for y, r in enumerate(m) for x, ch in enumerate(r) if ch == '#'}
+    return sum(manhattan_dist(m, expansion, g1, g2) for g1, g2 in combinations(galaxies, 2))
 
 
 if __name__ == "__main__":
