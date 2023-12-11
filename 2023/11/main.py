4115f198eb491a54c902869cd15b3d4850aa8466
shunlog <artiombalan331@gmail.com>
2023-12-12 00:13:19 +0200

    rewrite a bit

--- 2023/11/main.py
+++ 2023/11/main.py
@@ -4,10 +4,11 @@ from itertools import combinations
 from functools import cache
 
 @cache
-def compute_actual_dist(m, expansion):
+def coords_after_expansion(m, expansion):
     '''For any x and y on which there is a galaxy,
     actual_x[x] = actual x after expansion
     actual_y[y] = actual y after expansion
+    expansion: number of rows/columns to add after each empty one
     '''
     w, h = len(m[0]), len(m)
     actual_y = {}
@@ -29,18 +30,20 @@ def compute_actual_dist(m, expansion):
     return actual_x, actual_y
 
 
-def manhattan_dist(m, expansion, g1, g2):
-    actual_x, actual_y = compute_actual_dist(m, expansion)
+def dist(m, expansion, g1, g2):
+    '''Return the manhattan dist. between coords g1 and g2,
+    after each empty row/column has been expanded plus `expansion` times'''
+    actual_x, actual_y = coords_after_expansion(m, expansion)
     dx = abs(actual_x[g1[0]] - actual_x[g2[0]])
     dy = abs(actual_y[g1[1]] - actual_y[g2[1]])
     return dx + dy
 
 
 def solve(inp, part2=False):
-    expansion = 1000000-1 if part2 else 1
+    expansion = 10**6-1 if part2 else 1
     m = tuple(inp.splitlines())  # needs to be hashable
     galaxies = {(x, y) for y, r in enumerate(m) for x, ch in enumerate(r) if ch == '#'}
-    return sum(manhattan_dist(m, expansion, g1, g2) for g1, g2 in combinations(galaxies, 2))
+    return sum(dist(m, expansion, g1, g2) for g1, g2 in combinations(galaxies, 2))
 
 
 if __name__ == "__main__":
