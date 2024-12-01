6495759d9b1e3b3077fb5abd1953ae30e53c78c3
shunlog <artiombalan331@gmail.com>
2024-12-01 17:42:00 +0200

    dirty 2

--- 2024/01/main.py
+++ 2024/01/main.py
@@ -1,6 +1,6 @@
 #!/bin/env python3
-
 from icecream import ic
+from collections import Counter
 
 with open("my.in") as f:
     inp = f.read()
@@ -15,8 +15,6 @@ for l in inp.split('\n'):
     l1.append(int(a))
     l2.append(int(b))
 
-ic(l1, l2)
-
 ls1 = sorted(l1)
 ls2 = sorted(l2)
 
@@ -25,4 +23,13 @@ for x, y in zip(ls1, ls2):
     d = abs(x - y)
     s += d
 
+print(s)
+
+
+c = Counter(l2)
+ic(c)
+
+s = 0
+for x in l1:
+    s += x * c[x]
 print(s)
